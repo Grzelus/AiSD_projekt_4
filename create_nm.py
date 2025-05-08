40472ca1038f0e538dfa13e3cb2181081ef6daf2
Antoni Małecki <antmalecki>
2025-05-08 23:19:48 +0200

    create_nm added

---
+++ create_nm.py
@@ -0,0 +1,22 @@
+def printGraph(Tab):
+    for i in Tab:
+        print(i)
+
+def createNeighbourhoodMatrix(Tab):
+    A,V=map(int,Tab[0])
+    AvailableVertices=[]
+    for i in range(1,A+1):
+        AvailableVertices.append(i)
+
+    Graph=[[0 for x in range(A+1)]for y in range(A+1)]
+    
+    for i in range(1,V+1):
+        a,b=map(int,Tab[i])
+        if(a<=A and b<=A):
+            Graph[a][b]=1 
+            Graph[b][a]=1
+        else:
+            print(f"Out of range {a} and {b}")
+
+    printGraph(Graph)
+    return [Graph,AvailableVertices]
