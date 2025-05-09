7cd88e718756a8be20937bfca4d8b2d34897d005
Antoni <antmalecki@gmail.com>
2025-05-09 20:22:23 +0200

    fluery update still not working

--- fleury_nm.py
+++ fleury_nm.py
@@ -1,20 +1,27 @@
-def Fleury(Graph, Vertices,V):
+def FleuryAlgorithm(Graph, Vertices,V):
     amountOfVertices=len(Vertices)
-    cyclePath=[]
     GraphCopy=Graph
-    for _ in Vertices:
-        if ECycle(_):
-            print("Jest obwód Eulera")
+    
     def ECycle(startingPoint, actual):
+        cyclePath.append(actual)
+        print(f"{cyclePath} \n")
         for i in Vertices:
             if GraphCopy[actual][i]==1:
-                if len(cyclePath)==V and i==startingPoint:
+                if len(cyclePath)==V+1 and i==startingPoint:
+                    cyclePath.append(i)
                     return True
-                cyclePath.append(i)
-                GraphCopy[actual][i]=2
+                GraphCopy[actual][i]=0
+                GraphCopy[i][actual]=0
                 if ECycle(startingPoint,i):
                     return True
         
-        GraphCopy[len(cyclePath)-2][actual]=1
+        GraphCopy[cyclePath[len(cyclePath)-2]][actual]=1
+        GraphCopy[actual][cyclePath[len(cyclePath)-2]]=1
         return False
-    return 
+    for _ in Vertices:
+        cyclePath=[]
+        if ECycle(_,_):
+            print("Jest obwód Eulera")
+            return cyclePath
+    print("Obwód nie istnieje")
+    return None
